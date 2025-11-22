7307be4b143d10c039448d7ddade491026dc31b0
Tanmayi B V <tanmayibv2006@gmail.com>
2025-11-22 14:52:45 +0530

    feat(preprocess): create image preprocessing utilities

---
+++ src/preprocessing/image_utils.py
@@ -0,0 +1,23 @@
+﻿import numpy as np
+from PIL import Image
+from config.constants import IMAGE_SIZE
+
+def load_and_preprocess_image(image_path, target_size=IMAGE_SIZE):
+    '''Load and preprocess single image for prediction'''
+    img = Image.open(image_path).convert('RGB')
+    img = img.resize((target_size, target_size))
+    img_array = np.array(img) / 255.0
+    return np.expand_dims(img_array, axis=0)
+
+def preprocess_batch(image_paths, target_size=IMAGE_SIZE):
+    '''Preprocess batch of images'''
+    batch = []
+    for path in image_paths:
+        img = Image.open(path).convert('RGB')
+        img = img.resize((target_size, target_size))
+        batch.append(np.array(img) / 255.0)
+    return np.array(batch)
+
+def denormalize_image(img_array):
+    '''Convert normalized image back to 0-255 range'''
+    return (img_array * 255).astype(np.uint8)
