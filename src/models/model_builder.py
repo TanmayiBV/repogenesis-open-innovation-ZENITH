a915f5cadb7f970d22e29337f8590ba41a824be9
Tanmayi B V <tanmayibv2006@gmail.com>
2025-11-22 14:53:47 +0530

    feat(model): add custom classification head with dropout

---
+++ src/models/model_builder.py
@@ -0,0 +1,16 @@
+﻿from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Dropout
+from tensorflow.keras.models import Model
+
+def add_classification_head(base_model, num_classes):
+    '''Add custom classification layers on top of base model'''
+    x = base_model.output
+    x = GlobalAveragePooling2D(name='global_avg_pool')(x)
+    x = Dense(512, activation='relu', name='fc1')(x)
+    x = Dropout(0.5, name='dropout1')(x)
+    x = Dense(256, activation='relu', name='fc2')(x)
+    x = Dropout(0.3, name='dropout2')(x)
+    predictions = Dense(num_classes, activation='softmax', name='predictions')(x)
+    
+    model = Model(inputs=base_model.input, outputs=predictions)
+    print(f'✅ Classification head added: {num_classes} output classes')
+    return model
