4fda9a8739eb6d585295a8257551bc676bcca7f3
Tanmayi B V <tanmayibv2006@gmail.com>
2025-11-22 14:29:56 +0530

    feat(config): add OpenWeatherMap API configuration

---
+++ config/api_config.py
@@ -0,0 +1,14 @@
+﻿import os
+
+# OpenWeatherMap API
+WEATHER_API_KEY = '74742501843485e4ce8689655641104a'
+WEATHER_BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
+
+# Karnataka District Coordinates
+DISTRICT_COORDS = {
+    'Mandya': (12.5244, 76.8958),
+    'Bangalore': (12.9716, 77.5946),
+    'Mysore': (12.2958, 76.6394),
+    'Hubli': (15.3647, 75.1240),
+    # Add all 24 districts...
+}
