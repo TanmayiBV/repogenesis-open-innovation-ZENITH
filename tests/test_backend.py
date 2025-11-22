49736cad397d3ae641f3addd643fa3b0097031fe
Tanmayi B V <tanmayibv2006@gmail.com>
2025-11-22 15:16:48 +0530

    test: add unit tests for backend services

---
+++ tests/test_backend.py
@@ -0,0 +1,18 @@
+﻿import unittest
+from src.backend.crop_recommender import calculate_npk_match
+
+class TestCropRecommender(unittest.TestCase):
+    def test_npk_match_calculation(self):
+        soil = {'N': 200, 'P': 50, 'K': 50}
+        crop = {'N': 200, 'P': 50, 'K': 50}
+        score = calculate_npk_match(soil, crop)
+        self.assertEqual(score, 100.0)
+    
+    def test_npk_mismatch(self):
+        soil = {'N': 100, 'P': 25, 'K': 25}
+        crop = {'N': 200, 'P': 50, 'K': 50}
+        score = calculate_npk_match(soil, crop)
+        self.assertLess(score, 100.0)
+
+if __name__ == '__main__':
+    unittest.main()
