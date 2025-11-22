3b33fe29ff375ecbe57f379f299736d98df434dd
Tanmayi B V <tanmayibv2006@gmail.com>
2025-11-22 14:59:21 +0530

    refactor(backend): organize backend module exports

---
+++ src/backend/__init__.py
@@ -0,0 +1,21 @@
+﻿from src.backend.weather_service import get_weather_forecast
+from src.backend.crop_recommender import recommend_crops, calculate_npk_match
+from src.backend.market_intelligence import get_market_prices, get_price_comparison
+from src.backend.scheme_finder import get_government_schemes, find_schemes_by_farmer_profile
+from src.backend.profile_manager import save_profile, load_profile, get_profile_summary
+from src.backend.dashboard import generate_dashboard_data, calculate_farming_progress
+
+__all__ = [
+    'get_weather_forecast',
+    'recommend_crops',
+    'calculate_npk_match',
+    'get_market_prices',
+    'get_price_comparison',
+    'get_government_schemes',
+    'find_schemes_by_farmer_profile',
+    'save_profile',
+    'load_profile',
+    'get_profile_summary',
+    'generate_dashboard_data',
+    'calculate_farming_progress'
+]
