2c84f9604dabee43fe047ac3ac683fafdba647ed
Tanmayi B V <tanmayibv2006@gmail.com>
2025-11-22 14:30:26 +0530

    feat(config): define model hyperparameters and paths

---
+++ config/constants.py
@@ -0,0 +1,11 @@
+﻿# Model Configuration
+IMAGE_SIZE = 224
+BATCH_SIZE = 32
+EPOCHS = 15
+LEARNING_RATE = 0.0001
+
+# Dataset Path
+DATASET_PATH = '/kaggle/input/plantvillage-dataset/color/'
+
+# Model Save Path
+MODEL_SAVE_PATH = 'data/models/disease_detector_v1.keras'
